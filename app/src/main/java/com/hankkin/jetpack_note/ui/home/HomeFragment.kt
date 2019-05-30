a2d95ade3b8bbf366b1528a16ce2c5cbeb882ee0
alan <alan@cuizhigang.com>
2019-05-31 00:19:42 +0800

    init home

--- app/src/main/java/com/hankkin/jetpack_note/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hankkin/jetpack_note/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.hankkin.jetpack_note.databinding.FragmentHomeBinding
 import com.hankkin.jetpack_note.utils.InjectorUtils
 
